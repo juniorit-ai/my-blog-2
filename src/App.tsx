acc57256b9d80f8a2a84316be8094345a5112ca9
ezhomelabs <haiwei.he@gmail.com>
2023-07-08 07:40:59 +1000

    init

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import './App.css';
+
+function App() {
+ 
+
+  return (
+    <div className="app">
+      <div className="header">        
+          <h2>My Blog</h2>
+          <img src="trumpportraitt-632x800.jpg"></img>
+      </div>
+
+      <div className="content">
+        Learn coding is so easy!
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
