9a7f021a94017b43bb12accd6ccd5e5b0520a8a0
ezhomelabs <haiwei.he@gmail.com>
2023-07-10 18:34:49 +1000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
+
+  const [visitorCount, setVisitorCount] = useState(0);
+
+  useEffect(() => {
+    let count = 0;
+    const visitorCount = localStorage.getItem("visitorCount");
+
+    if (visitorCount) {
+      count = parseInt(visitorCount);
+    }
+
+    count++;
+
+    localStorage.setItem("visitorCount", count.toString());
+    setVisitorCount(count);
+  }, []);
 
 
   return (
     <div className="app">
       <div className="header">        
           <h2>My Blog</h2>
-          <img src="trumpportraitt-632x800.jpg"></img>
+          <img src="my-img.png"></img>
       </div>
 
       <div className="content">
-           Donald John Trump is an American politician, media personality, and businessman who served as the 45th president of the United States from 2017 to 2021. Trump graduated from the Wharton School of the University of Pennsylvania with a bachelor's degree in 1968.
+        Write something here
       </div>
+
+      <div>Visitor Count: {visitorCount}</div>
+      
     </div>
   );
 }
